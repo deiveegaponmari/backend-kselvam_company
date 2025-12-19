88ef31128a4ae5b513a503139048ae8f2f43ef04
deiveegaponmari <ponmarimdeiveega@gmail.com>
2025-12-19 10:31:56 +0530

    event booking authorization completed.

--- routes/EventRoute.js
+++ routes/EventRoute.js
@@ -1,7 +1,8 @@
 const express=require('express');
 const { EventBooking }=require("../controllers/EventBookingController")
 const EventRouter=express.Router();
+const authMiddleware=require('../middlewares/authMiddleware')
 
-EventRouter.post("/event-book",EventBooking)
+EventRouter.post("/event-book",authMiddleware,EventBooking)
 
 module.exports=EventRouter;
