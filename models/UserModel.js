224abd9577ca02e72ec303fa9518cd34b269d1cf
deiveegaponmari <ponmarimdeiveega@gmail.com>
2025-12-22 12:24:25 +0530

    changes added

--- models/UserModel.js
+++ models/UserModel.js
@@ -27,12 +27,11 @@ const UserSchema = new mongoose.Schema(
 );
 
 //hash the password before saving
-UserSchema.pre("save", async function (next) {
+UserSchema.pre("save", async function () {
   if (this.isModified("password")) {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
   }
-  next();
 });
 
 const UserModel = mongoose.model("Users", UserSchema);
